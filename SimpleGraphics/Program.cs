a39f04b20d6496c153e5a6872fcb31de46bef804
Kraaven <dhruv.bhojaraj@gmail.com>
2025-06-28 21:41:34 +0530

    random colors on the screen

--- SimpleGraphics/Program.cs
+++ SimpleGraphics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Numerics;
 using Silk.NET.Input;
 using Silk.NET.Maths;
@@ -8,6 +9,10 @@ using Silk.NET.Windowing;
 public class Core
 {
     private static IWindow _window;
+    private static GL _gl;
+    private static Random _random = new Random();
+
+    private static int _index = 0;
     
     public static void Main()
     {
@@ -18,24 +23,42 @@ public class Core
             Size = new Vector2D<int>(1600, 1000),
             Title = "Simple Graphics Renderer"
         };
-        
         _window = Window.Create(options);
+        
+        //Window Subscribed runtime functions
         _window.Load += OnLoad;
         _window.Update += OnUpdate;
         _window.Render += OnRender;
 
+        //Window execution
         _window.Run();
     }
 
     private static void OnLoad()
     {
+        //Input Binding
         IInputContext input = _window.CreateInput();
         for (int i = 0; i < input.Keyboards.Count; i++) input.Keyboards[i].KeyDown += KeyDown;
+        
+        //Window OpenGL stuff
+        _gl = _window.CreateOpenGL();
+        _gl.ClearColor(Color.CornflowerBlue);
     }
 
-    private static void OnUpdate(double deltaTime) { }
+    private static void OnUpdate(double deltaTime)
+    {
+        if (_index == 20)
+        {
+            _gl.ClearColor(Color.FromArgb(1, _random.Next() % 255, _random.Next() % 255, _random.Next() % 255));
+            _index = 0;
+        }
+        else _index++;
+    }
 
-    private static void OnRender(double deltaTime) { }
+    private static void OnRender(double deltaTime)
+    {
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
+    }
 
     private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
     {
